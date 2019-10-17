7412467e2cab33f48a0b1f70da0838ec47209f40
gp6 <pfgao@ips.com>
2019-10-17 15:46:16 +0800

    Ribbon自定义负载规则

--- parent/mall-cloud-web/src/main/java/com/gp6/cloud/web/ribbon/RibbonConfig.java
+++ parent/mall-cloud-web/src/main/java/com/gp6/cloud/web/ribbon/RibbonConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 public class RibbonConfig {
 
     /**
-     * Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具。
+     * Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端负载均衡的工具。
      *
      * @return RestTemplate
      */
@@ -29,16 +29,12 @@ public class RibbonConfig {
 
     @Bean
     public IRule myRule() {
-        //return new RoundRobinRule();
-        //return new RandomRule();//达到的目的，用我们重新选择的随机算法替代默认的轮询。
+        // 轮询
+        // return new RoundRobinRule();
+        // 随机
+        // return new RandomRule();
+        // 重试
         return new RetryRule();
     }
 }
 
-//@Bean
-//public UserServcie getUserServcie()
-//{
-//	return new UserServcieImpl();
-//}
-// applicationContext.xml == ConfigBean(@Configuration)
-//<bean id="userServcie" class="com.atguigu.tmall.UserServiceImpl">
