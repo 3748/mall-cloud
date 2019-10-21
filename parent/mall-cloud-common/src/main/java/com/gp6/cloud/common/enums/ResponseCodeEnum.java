5ab8272c61a708057b9a5828e90f6676162862ac
gp6 <pfgao@ips.com>
2019-10-21 15:09:06 +0800

    依赖管理

--- parent/mall-cloud-common/src/main/java/com/gp6/cloud/common/enums/ResponseCodeEnum.java
+++ parent/mall-cloud-common/src/main/java/com/gp6/cloud/common/enums/ResponseCodeEnum.java
@@ -12,15 +12,14 @@ public enum ResponseCodeEnum {
     REQUEST_PARAM_ERROR(10001, "参数错误!"),
     CAST_ERROR(10002, "类型转换错误!"),
 
-
     MEMBER_REGISTER_SUCCESS(20000, "会员注册成功!"),
     MEMBER_REGISTER_FAIL(20001, "会员注册失败!"),
-    MEMBER_HYSTRIX(20002, "会员服务--熔断!"),
+    MEMBER_HYSTRIX(20002, "会员服务--降级!"),
+
 
     MQ_MESSAGE_TYPE_ERROR(30000, "MQ消息类型错误!"),
-    MQ_MESSAGE_CONTENT_NOT_BLANK(30001, "MQ消息不能为空!"),
+    MQ_MESSAGE_CONTENT_NOT_BLANK(30001, "MQ消息不能为空!")
     ;
-
 
     /**
      * 返回状态码
